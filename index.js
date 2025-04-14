6ef3932271e8a401f0e734d14a43939fbd5ef36c
Arssh Bajpai <125165541+Arssh-Bajpai@users.noreply.github.com>
2025-04-14 02:38:36 -0400

    a5

--- index.js
+++ index.js
@@ -8,49 +8,36 @@ import UserRoutes from "./Kambaz/Users/routes.js";
 import CourseRoutes from "./Kambaz/Courses/routes.js";
 import ModuleRoutes from "./Kambaz/Modules/routes.js";
 import AssignmentRoutes from './Kambaz/Assignments/routes.js';
-
 const app = express()
-
-// Set Access-Control-Allow-Origin header explicitly
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://symphonious-crostata-4f782f.netlify.app"); // Set the allowed origin (Netlify link)
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Credentials", "true"); // Allow credentials (cookies)
-
-  next(); // Pass to the next middleware
-});
-
-// Existing CORS configuration
 app.use(
-  cors({
-    credentials: true,
-    origin: process.env.NETLIFY_URL || "http://localhost:5173", // Default for local dev
-  })
-);
+    cors({
+      credentials: true,
+      origin: process.env.NETLIFY_URL || "http://localhost:5173",
 
-const sessionOptions = {
-  secret: process.env.SESSION_SECRET || "kambaz",
-  resave: false,
-  saveUninitialized: false,
-};
-
-if (process.env.NODE_ENV !== "development") {
-  sessionOptions.proxy = true;
-  sessionOptions.cookie = {
-    sameSite: "none",
-    secure: true,
-    domain: process.env.NODE_SERVER_DOMAIN,
+    })
+   );
+   const sessionOptions = {
+    secret: process.env.SESSION_SECRET || "kambaz",
+    resave: false,
+    saveUninitialized: false,
   };
-}
-
-Lab5(app);
-Hello(app);
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+      domain: process.env.NODE_SERVER_DOMAIN,
+    };
+  }
+  
+  
+  
+Lab5(app)
+Hello(app)
 app.use(session(sessionOptions));
 app.use(express.json());
 UserRoutes(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
-
-app.listen(process.env.PORT || 4000);
+app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}!`));
