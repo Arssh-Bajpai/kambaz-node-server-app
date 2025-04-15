1ff6ea69a84ae41132485c4c6c24ce734d23490a
Arssh Bajpai <125165541+Arssh-Bajpai@users.noreply.github.com>
2025-04-15 12:14:57 -0400

    a5

--- index.js
+++ index.js
@@ -40,4 +40,4 @@ UserRoutes(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
-app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}!`));
+app.listen(process.env.PORT || 4000);
